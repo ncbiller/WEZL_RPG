cd72f3c66059ac22fdea9c25e7bcfbbd375bbf33
Nicholas Biller <nick@biller.me.uk>
2017-10-14 19:10:03 +0100

    added existing RPG unity project

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(player.ToString());
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float x = player.transform.position.x;
+        float z = player.transform.position.y;
+        print(x);
+
+        transform.position = player.transform.position;
+    }
+
+    
+}
