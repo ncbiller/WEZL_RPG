9300af7f1e6681f518c34617ab7a245802c0755e
Nicholas Biller <nick@biller.me.uk>
2017-10-15 17:59:39 +0100

    Implement basic Layer Raycasting - Removed unused camera assets

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cr;
+    // Use this for initialization
+    void Start () {
+        cr = FindObjectOfType<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(cr.layerHit);
+	}
+}
