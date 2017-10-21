52ef1614114b3efe9635d147286fbc841c0a6cd2
Nicholas Biller <ncbiller@gmail.com>
2017-10-21 07:56:10 +0100

    Added cursor affordances - Project Tidy

---
+++ Assets/Camera and UI/CursorAffordance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D attackCursor = null;
+    [SerializeField] Texture2D errorCursor = null;
+
+    [SerializeField] Vector2 cursorOffset = new Vector2(96, 96);
+
+    Texture2D cursorTexture;
+
+    CameraRaycaster cameraRaycaster;
+    // Use this for initialization
+    void Start () {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                cursorTexture = walkCursor;
+                break;
+            case Layer.Enemy:
+
+                cursorTexture = attackCursor;
+                break;
+
+            default:
+                cursorTexture = errorCursor;
+                break;
+        }
+
+        Cursor.SetCursor(cursorTexture, cursorOffset, CursorMode.Auto);
+        print(cameraRaycaster.layerHit);
+       
+	}
+}
